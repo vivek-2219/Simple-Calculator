6e52029fc904eddc20e307106c7ffd5876976359
Vivek <thevivekkumar1234@gmail.com>
2022-05-03 13:00:27 +0530

    2nd commit

--- logic.js
+++ logic.js
@@ -9,22 +9,24 @@ evaluator.addEventListener('click', () => {
 
 lastValueSlicer.addEventListener('click', () => {
     output.value = output.value.slice(0, output.value.length - 1);
-})
+});
 
 operators.forEach(element => {
     element.addEventListener('click', () => {
         if (output.value.charAt(output.value.length - 2) === '+') {
             output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        }
+        };
         if (output.value.charAt(output.value.length - 2) === '-') {
             output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        }
+        };
         if (output.value.charAt(output.value.length - 2) === 'X') {
             output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        }
+        };
         if (output.value.charAt(output.value.length - 2) === '/') {
             output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        }
+        };
     });
 });
+
+
 
