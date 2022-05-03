7f8f6b8f9ae6bb34941b41adabe67c9304096e22
Vivek <thevivekkumar1234@gmail.com>
2022-05-03 14:33:02 +0530

    0 values fixed

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@ let operators = document.querySelectorAll('.operators');
 let output = document.querySelector('.output');
 let evaluator = document.querySelector('.evaluator');
 let lastValueSlicer = document.querySelector('.lastValueSlicer');
+let numbers = document.querySelectorAll('.numbers');
 
 evaluator.addEventListener('click', () => {
     output.value = eval(output.value.replaceAll('X', '*'));
@@ -9,6 +10,19 @@ evaluator.addEventListener('click', () => {
 
 lastValueSlicer.addEventListener('click', () => {
     output.value = output.value.slice(0, output.value.length - 1);
+});
+
+numbers.forEach(element => {
+    element.addEventListener('click', () => {
+        if (output.value.length > 1) {
+            if (output.value.startsWith(0)) {
+                output.value = output.value.slice(1);
+            };
+            if (output.value.startsWith(00)) {
+                output.value = output.value.slice(2);
+            };
+        };
+    });
 });
 
 operators.forEach(element => {
