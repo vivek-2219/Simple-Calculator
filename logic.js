7ede6f7e0f18d1636f16b51ba6015a7324e286be
Vivek <thevivekkumar1234@gmail.com>
2022-05-07 10:30:26 +0530

    Trigonometric calculator functionalities added

--- logic.js
+++ logic.js
@@ -3,9 +3,24 @@ let output = document.querySelector('.output');
 let evaluator = document.querySelector('.evaluator');
 let lastValueSlicer = document.querySelector('.lastValueSlicer');
 let numbers = document.querySelectorAll('.numbers');
+let mathematicalOperators = ['+', '-', '/'];
 
 evaluator.addEventListener('click', () => {
-    output.value = eval(output.value.replaceAll('X', '*'));
+    let functions = ['sin', 'cos', 'tan', 'cosec', 'sec', 'cot', 'asin', 'acos', 'atan', 'acosec', 'asec', 'acot'];
+    functions.forEach(element => {
+        let trigoFunc = 'Math.' + element;
+        output.value = output.value.replaceAll(element, trigoFunc);
+        output.value = output.value.replaceAll('Math.cosec', '1/Math.sin');
+        output.value = output.value.replaceAll('Math.sec', '1/Math.cos');
+        output.value = output.value.replaceAll('Math.cot', '1/Math.tan');
+        output.value = output.value.replaceAll('aMath.', 'a');
+        output.value = output.value.replaceAll('a1/Math.sin(', 'asin(1/');
+        output.value = output.value.replaceAll('a1/Math.cos(', 'acos(1/');
+        output.value = output.value.replaceAll('a1/Math.tan(', 'atan(1/');
+    });
+    console.log(output.value);
+    let result = eval(output.value.replaceAll('X', '*'));
+    output.value = result;
 });
 
 lastValueSlicer.addEventListener('click', () => {
@@ -27,18 +42,11 @@ numbers.forEach(element => {
 
 operators.forEach(element => {
     element.addEventListener('click', () => {
-        if (output.value.charAt(output.value.length - 2) === '+') {
-            output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        };
-        if (output.value.charAt(output.value.length - 2) === '-') {
-            output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        };
-        if (output.value.charAt(output.value.length - 2) === 'X') {
-            output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        };
-        if (output.value.charAt(output.value.length - 2) === '/') {
-            output.value = output.value.slice(0, output.value.length - 2) + element.value;
-        };
+        mathematicalOperators.forEach(operand => {
+            if (output.value.charAt(output.value.length - 2) === operand) {
+                output.value = output.value.slice(0, output.value.length - 2) + element.value;
+            };
+        });
     });
 });
 
